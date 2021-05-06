21276f2ffe76ad2d5de22cdb74d141be9c0b7bb3
Ivan <duremar98@yandex.ru>
2021-05-06 22:38:23 +0300

    log

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,33 @@ import "./App.css";
 
 const App = () => {
   const [page, setPage] = useState(1);
+  const [touchStart, setTouchStart] = useState(0);
+  const [touchEnd, setTouchEnd] = useState(0);
+
   console.log(page);
+
+  const handleTouchStart = (e) => {
+    setTouchStart(e.targetTouches[0].clientY);
+  };
+
+  const handleTouchMove = (e) => {
+    setTouchEnd(e.targetTouches[0].clientY);
+  };
+
+  const handleTouchEnd = () => {
+    if (touchStart - touchEnd > 50) {
+      console.log("botton");
+      setTouchStart(0);
+      setTouchEnd(0);
+    }
+
+    if (touchStart - touchEnd < -50) {
+      console.log("top");
+      setTouchStart(0);
+      setTouchEnd(0);
+    }
+  };
+
   const ChangePage = (next) => {
     if (next) {
       setPage((prevPage) => (prevPage === 3 ? 1 : prevPage + 1));
@@ -15,22 +41,40 @@ const App = () => {
     }
   };
 
-  const Number = (page) => {
+  const Pages = (page) => {
     const pages = [<Page1 />, <Page2 />, <Page3 />];
     return pages.map((item, index) => {
-      if (index === page - 1) {
-        return <div key={index}>{item}</div>;
-      } else {
-        return <div key={index}>0000</div>;
-      }
+      const classPage = `page${index + 1}`;
+      return (
+        <div
+          key={index}
+          id={index + 1}
+          className={classPage + " page active_page"}
+          onTouchStart={(touchStartEvent) => handleTouchStart(touchStartEvent)}
+          onTouchMove={(touchMoveEvent) => handleTouchMove(touchMoveEvent)}
+          onTouchEnd={() => handleTouchEnd()}
+          onPointerEnter={(e) =>
+            setPage((prevPage) =>
+              prevPage !== e.target.id ? e.target.id : prevPage
+            )
+          }
+          // onTouchMove={(e) => console.log(e.target.id)}
+        >
+          {item}
+        </div>
+      );
     });
   };
 
   return (
-    <div className="Wrapper">
-      {Number(page)}
-      <button onClick={() => ChangePage()}>prev</button>
-      <button onClick={() => ChangePage("next")}>next</button>
+    <div className="wrapper">
+      {Pages(page)}
+      <button className="prev" onClick={() => ChangePage()}>
+        prev
+      </button>
+      <button className="next" onClick={() => ChangePage("next")}>
+        next
+      </button>
     </div>
   );
 };
