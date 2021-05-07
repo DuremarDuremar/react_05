d9626bd4a0204762f40fb2aa5a224eab2ecaf269
Ivan <duremar98@yandex.ru>
2021-05-07 21:02:28 +0300

    log

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Page1 from "./components/page1";
 import Page2 from "./components/page2";
 import Page3 from "./components/page3";
@@ -8,8 +8,21 @@ const App = () => {
   const [page, setPage] = useState(1);
   const [touchStart, setTouchStart] = useState(0);
   const [touchEnd, setTouchEnd] = useState(0);
-
+  const myRef1 = useRef(null);
+  const myRef2 = useRef(null);
+  const myRef3 = useRef(null);
   console.log(page);
+  console.log("touchStart", touchStart);
+  console.log("touchEnd", touchEnd);
+
+  const scrollElement = () => {
+    myRef2.current.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+    setTouchStart(0);
+    setTouchEnd(0);
+  };
 
   const handleTouchStart = (e) => {
     setTouchStart(e.targetTouches[0].clientY);
@@ -21,15 +34,17 @@ const App = () => {
 
   const handleTouchEnd = () => {
     if (touchStart - touchEnd > 50) {
-      console.log("botton");
-      setTouchStart(0);
-      setTouchEnd(0);
+      if (touchStart !== 0 && touchEnd !== 0) {
+        scrollElement();
+        console.log("botton");
+      }
     }
 
     if (touchStart - touchEnd < -50) {
-      console.log("top");
-      setTouchStart(0);
-      setTouchEnd(0);
+      if (touchStart !== 0 && touchEnd !== 0) {
+        scrollElement();
+        console.log("top");
+      }
     }
   };
 
@@ -41,24 +56,28 @@ const App = () => {
     }
   };
 
-  const Pages = (page) => {
+  const Pages = () => {
     const pages = [<Page1 />, <Page2 />, <Page3 />];
     return pages.map((item, index) => {
       const classPage = `page${index + 1}`;
+      const classNone =
+        (page == 1 && index == 2) || (page == 3 && index == 0)
+          ? "noactive_page"
+          : "active_page";
       return (
         <div
           key={index}
           id={index + 1}
-          className={classPage + " page active_page"}
-          onTouchStart={(touchStartEvent) => handleTouchStart(touchStartEvent)}
-          onTouchMove={(touchMoveEvent) => handleTouchMove(touchMoveEvent)}
+          ref={index === 0 ? myRef1 : index === 1 ? myRef2 : myRef3}
+          className={classPage + " page " + classNone}
+          onTouchStart={(e) => handleTouchStart(e)}
+          onTouchMove={(e) => handleTouchMove(e)}
           onTouchEnd={() => handleTouchEnd()}
           onPointerEnter={(e) =>
             setPage((prevPage) =>
               prevPage !== e.target.id ? e.target.id : prevPage
             )
           }
-          // onTouchMove={(e) => console.log(e.target.id)}
         >
           {item}
         </div>
