e251f853ba87ff473ee1d6a37b5e623e99d24c76
Ivan <duremar98@yandex.ru>
2021-05-08 22:18:52 +0300

    style

---
+++ src/components/circles.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./circles.css";
+
+export const circle1 = <div className="circle circle1"></div>;
+export const circle2 = <div className="circle circle2"></div>;
+export const circle3 = <div className="circle circle3"></div>;
+export const circle4 = <div className="circle circle4"></div>;
