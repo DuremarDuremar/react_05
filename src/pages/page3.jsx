08ca799ead0a7b2c102614cc48205ce61e11237b
Ivan <duremar98@yandex.ru>
2021-05-11 01:48:25 +0300

    style

--- src/pages/page3.jsx
+++ src/pages/page3.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import beta from "../images/beta.png";
+import "./page3.css";
 
 const Page3 = () => {
-  return <div>3</div>;
+  return (
+    <section className="page3_content">
+      <div className="page3_wrapper">
+        <h1 className="page3_title">Звенья патогенеза СД2</h1>
+        <div className="page3_name1">
+          <strong>1&nbsp;β-клетки</strong>
+          <div className="page3_beta">
+            <img className="page3_beta-img" src={beta} alt="5" />
+          </div>
+          <div className="page3_arrow-1">
+            <div className="page3_arrow-1_"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Page3;
