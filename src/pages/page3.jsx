1cb778addfc05d618e7571e47e5c0dae10f66990
Ivan <duremar98@yandex.ru>
2021-05-12 03:07:38 +0300

    style

--- src/pages/page3.jsx
+++ src/pages/page3.jsx
@@ -1,12 +1,29 @@
 import React from "react";
-import { Arrow } from "../components/arrow";
-import beta from "../images/beta.png";
+import { Content, Slide1, Slide2, Slide3, Title } from "./style/style3";
+// import { Arrow } from "../components/arrow";
+// import beta from "../images/beta.png";
 import "./page3.css";
 
 const Page3 = () => {
   return (
-    <section className="page3_content">
-      <div className="page3_wrapper">
+    <Content>
+      <Slide1>
+        <Title>1</Title>
+      </Slide1>
+      <Slide2>
+        <Title>2</Title>
+      </Slide2>
+      <Slide3>
+        <Title>3</Title>
+      </Slide3>
+    </Content>
+  );
+};
+
+export default Page3;
+
+{
+  /* <div className="page3_wrapper">
         <h1 className="page3_title">Звенья патогенеза СД2</h1>
         <div className="page3_name1">
           <strong>1&nbsp;β-клетки</strong>
@@ -15,9 +32,5 @@ const Page3 = () => {
             <Arrow t2 />
           </div>
         </div>
-      </div>
-    </section>
-  );
-};
-
-export default Page3;
+      </div> */
+}
---
+++ src/pages/style/style3.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Content = styled.section`
+  height: 100%;
+  display: flex;
+  /* overflow: hidden; */
+  transform: translate(-820px);
+`;
+export const Slide1 = styled.section`
+  background-color: azure;
+  min-width: 768px;
+  width: 100%;
+`;
+export const Slide2 = styled.section`
+  background-color: blueviolet;
+  min-width: 768px;
+  width: 100%;
+`;
+export const Slide3 = styled.section`
+  background-color: saddlebrown;
+  min-width: 768px;
+  width: 100%;
+`;
+export const Title = styled.section``;
