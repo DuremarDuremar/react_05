a98b5530fe1d769a06f74e6b7064a94083c66cb0
Ivan <duremar98@yandex.ru>
2021-05-12 19:10:32 +0300

    style

--- src/pages/page3.jsx
+++ src/pages/page3.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-import { Content, Slide1, Slide2, Slide3, Title } from "./style/style3";
-// import { Arrow } from "../components/arrow";
-// import beta from "../images/beta.png";
+import {
+  Content,
+  Slide1,
+  Slide2,
+  Slide3,
+  Title,
+  WrapperImg,
+} from "./style/style3";
+import beta from "../images/beta.png";
+import brain from "../images/brain.png";
+import capa from "../images/capa.png";
+import infection from "../images/infection.png";
+import kidneys from "../images/kidneys.png";
+import lipid from "../images/lipid.png";
+import microflora from "../images/microflora.png";
+import musle from "../images/musle.png";
+import stomach from "../images/stomach.png";
 import "./page3.css";
 
 const Page3 = () => {
@@ -14,7 +28,34 @@ const Page3 = () => {
         <Title>2</Title>
       </Slide2>
       <Slide3>
-        <Title>3</Title>
+        <Title>Звенья патогенеза СД2</Title>
+        <WrapperImg width="35px" height="45px" top="180px" left="380px">
+          <img src={beta} alt="beta" />
+        </WrapperImg>
+        <WrapperImg width="55px" height="55px" top="240px" left="580px">
+          <img src={brain} alt="brain" />
+        </WrapperImg>
+        <WrapperImg>
+          <img src={capa} alt="capa" />
+        </WrapperImg>
+        <WrapperImg>
+          <img src={infection} alt="infection" />
+        </WrapperImg>
+        <WrapperImg>
+          <img src={kidneys} alt="kidneys" />
+        </WrapperImg>
+        <WrapperImg>
+          <img src={lipid} alt="lipid" />
+        </WrapperImg>
+        <WrapperImg>
+          <img src={microflora} alt="microflora" />
+        </WrapperImg>
+        <WrapperImg>
+          <img src={musle} alt="musle" />
+        </WrapperImg>
+        <WrapperImg>
+          <img src={stomach} alt="stomach" />
+        </WrapperImg>
       </Slide3>
     </Content>
   );
--- src/pages/style/style3.jsx
+++ src/pages/style/style3.jsx
@@ -3,9 +3,9 @@ import styled from "styled-components";
 export const Content = styled.section`
   height: 100%;
   display: flex;
-  /* overflow: hidden; */
-  transform: translate(-820px);
+  transform: translate(-1536px);
 `;
+
 export const Slide1 = styled.section`
   background-color: azure;
   min-width: 768px;
@@ -20,5 +20,27 @@ export const Slide3 = styled.section`
   background-color: saddlebrown;
   min-width: 768px;
   width: 100%;
+  position: relative;
 `;
-export const Title = styled.section``;
+export const Title = styled.h1`
+  font-family: "Gotham Regular", "Arial", sans-serif;
+  margin: 0;
+  padding: 100px 0 0 50px;
+  font-size: 28px;
+  font-weight: normal;
+  line-height: 1.2;
+  letter-spacing: -0.6px;
+  color: #fff;
+  object-fit: contain;
+`;
+
+export const WrapperImg = styled.div`
+  position: absolute;
+  width: ${(props) => props.width || "0"};
+  height: ${(props) => props.height || "0"};
+  top: ${(props) => props.top || "0"};
+  left: ${(props) => props.left || "0"};
+  img {
+    width: 100%;
+  }
+`;
