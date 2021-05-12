c0ac571832e32168c259d44986a30802c47e9007
Ivan <duremar98@yandex.ru>
2021-05-12 22:35:53 +0300

    style

--- src/pages/page3.jsx
+++ src/pages/page3.jsx
@@ -5,7 +5,10 @@ import {
   Slide2,
   Slide3,
   Title,
+  TextImg,
+  FonImg,
   WrapperImg,
+  BlockImg,
 } from "./style/style3";
 import beta from "../images/beta.png";
 import brain from "../images/brain.png";
@@ -29,15 +32,65 @@ const Page3 = () => {
       </Slide2>
       <Slide3>
         <Title>Звенья патогенеза СД2</Title>
-        <WrapperImg width="35px" height="45px" top="180px" left="380px">
-          <img src={beta} alt="beta" />
+        <WrapperImg top="180px" left="360px" display="block">
+          <TextImg fs="26px">
+            <b>1</b> β-клетки
+          </TextImg>
+          <FonImg
+            padding="13px 11px 6px"
+            width="85px"
+            height="85px"
+            widthImg="80%"
+            heightImg="80%"
+          >
+            <img src={beta} alt="beta" />
+          </FonImg>
         </WrapperImg>
-        <WrapperImg width="55px" height="55px" top="240px" left="580px">
-          <img src={brain} alt="brain" />
+        <WrapperImg top="260px" left="540px">
+          <FonImg widthImg="70%" heightImg="70%">
+            <img src={brain} alt="brain" />
+          </FonImg>
+          <TextImg>
+            <b>7</b>
+            <br />
+            Головной мозг
+          </TextImg>
         </WrapperImg>
-        <WrapperImg>
-          <img src={capa} alt="capa" />
-        </WrapperImg>
+
+        <BlockImg>
+          <h2>Инсулинорезистентность</h2>
+          <WrapperImg position="relative">
+            <FonImg padding="15px 11px 1px" widthImg="80%" heightImg="80%">
+              <img src={capa} alt="capa" />
+            </FonImg>
+            <TextImg>
+              <b>6</b>
+              <br />
+              Печень
+            </TextImg>
+          </WrapperImg>
+          <WrapperImg position="relative">
+            <FonImg widthImg="90%" heightImg="90%">
+              <img src={musle} alt="musle" />
+            </FonImg>
+            <TextImg>
+              <b>5</b>
+              <br />
+              Мыщцы
+            </TextImg>
+          </WrapperImg>
+          <WrapperImg position="relative">
+            <FonImg widthImg="80%" heightImg="80%">
+              <img src={lipid} alt="lipid" />
+            </FonImg>
+            <TextImg>
+              <b>4</b>
+              <br />
+              Жировые клетки
+            </TextImg>
+          </WrapperImg>
+        </BlockImg>
+
         <WrapperImg>
           <img src={infection} alt="infection" />
         </WrapperImg>
@@ -45,13 +98,7 @@ const Page3 = () => {
           <img src={kidneys} alt="kidneys" />
         </WrapperImg>
         <WrapperImg>
-          <img src={lipid} alt="lipid" />
-        </WrapperImg>
-        <WrapperImg>
           <img src={microflora} alt="microflora" />
-        </WrapperImg>
-        <WrapperImg>
-          <img src={musle} alt="musle" />
         </WrapperImg>
         <WrapperImg>
           <img src={stomach} alt="stomach" />
--- src/pages/style/style3.jsx
+++ src/pages/style/style3.jsx
@@ -17,11 +17,11 @@ export const Slide2 = styled.section`
   width: 100%;
 `;
 export const Slide3 = styled.section`
-  background-color: saddlebrown;
   min-width: 768px;
   width: 100%;
   position: relative;
 `;
+
 export const Title = styled.h1`
   font-family: "Gotham Regular", "Arial", sans-serif;
   margin: 0;
@@ -34,13 +34,67 @@ export const Title = styled.h1`
   object-fit: contain;
 `;
 
-export const WrapperImg = styled.div`
+export const BlockImg = styled.div`
+  border-top-left-radius: 70px;
+  border-top-right-radius: 70px;
   position: absolute;
-  width: ${(props) => props.width || "0"};
-  height: ${(props) => props.height || "0"};
+  top: 350px;
+  left: 500px;
+  width: 240px;
+  height: 260px;
+  background: rgb(64, 65, 98);
+  background: linear-gradient(
+    to bottom,
+    rgba(64, 65, 98, 1) 36%,
+    rgba(26, 25, 115, 1) 100%
+  );
+  border-top: solid 2px #030d22;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-left: 20px;
+  h2 {
+    font-size: 15px;
+    font-weight: 500;
+    letter-spacing: -0.3px;
+    text-align: center;
+    color: #fff;
+    margin-top: 15px;
+  }
+`;
+
+export const WrapperImg = styled.div`
+  position: ${(props) => props.position || "absolute"};
   top: ${(props) => props.top || "0"};
   left: ${(props) => props.left || "0"};
+  display: ${(props) => props.display || "flex"};
+`;
+
+export const TextImg = styled.h3`
+  font-family: Lato;
+  font-weight: 500;
+  font-size: ${(props) => props.fs || "20px"};
+  margin: ${(props) => (props.fs ? "0 0 8px 0" : "0 0 0 8px")};
+  letter-spacing: -0.51px;
+  text-align: left;
+  color: #fff;
+  b {
+    font-size: 30px;
+  }
+`;
+
+export const FonImg = styled.div`
+  background-color: #404162;
+  border-radius: 100%;
+  width: ${(props) => props.width || "60px"};
+  height: ${(props) => props.height || "60px"};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   img {
-    width: 100%;
+    padding: ${(props) => props.padding || "0"};
+    max-width: ${(props) => props.widthImg || "100%"};
+    max-height: ${(props) => props.heightImg || "100%"};
   }
 `;
